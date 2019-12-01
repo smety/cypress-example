9fa2ca0f7e32ce5df393bdeaa369dd2f9674d0e8
Tomas Smetka <me@smetka.name>
2019-12-01 21:59:07 +0100

    Create sample test / more fixes

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,14 +1,5 @@
 window.addEventListener('load', function () {
 
   /** Open & close box **/
-  document.getElementById('js-box-open').addEventListener('click', function (e) {
-    e.preventDefault();
-    document.getElementById('js-box').classList.toggle('visible')
-  })
 
-  /** Change title **/
-  document.getElementById('js-change-title').addEventListener('click', function (e) {
-    e.preventDefault();
-    document.title = 'My custom title'
-  })
 })
